dc8b60e79392ae26ff5e311f77181ec4a954bbd9
Fronx <fronx@soundcloud.com>
2014-07-16 19:37:27 +0200

    introduce `dyn`!!

--- Unityped.hs
+++ Unityped.hs
@@ -8,19 +8,18 @@ import qualified Prelude as P
 data D = B Bool
        | N Int
        | S String
-       | F  (D -> D)
+       | F (D -> D)
        | F2 ((D, D) -> D)
-       | Object [(String, D)]
 
 print d = putStrLn s
   where S s = show $$ d
 
 show = F f
-  where f (B True)   = S "true"
-        f (B False)  = S "false"
-        f (N n)      = S (P.show n)
-        f (S s)      = S s
-        f (F _)      = S "(function)"
+  where f (B True)   = dyn "true"
+        f (B False)  = dyn "false"
+        f (N n)      = dyn (P.show n)
+        f (S s)      = dyn s
+        f (F _)      = dyn "(function)"
 
 class Apply argType where
   ($$) :: D -> argType -> D
@@ -31,20 +30,31 @@ instance Apply D where
 instance Apply (D, D) where
   (F2 f) $$ (d, d') = f (d, d')
 
-mul = F2 f
+mul = dyn f
   where f (x    , (N 1)) = x
-        f ((N n), (N m)) = N (n P.* m)
-        f ((S s), (N m)) = mul $$ ((S (s ++ s)), (N (m - 1)))
-        f ((F g), (N m)) = mul $$ ((F (g .  g)), (N (m - 1)))
+        f ((N n), (N m)) = dyn (n P.* m)
+        f ((S s), (N m)) = mul $$ ((dyn (s ++ s)), (dyn (m - 1)))
+        f ((F g), (N m)) = mul $$ ((dyn (g .  g)), (dyn (m - 1)))
 
 a * b = mul $$ (a, b)
 
+class Dyn a where
+  dyn :: a -> D
+
+instance Dyn Bool     where dyn = B
+instance Dyn Int      where dyn = N
+instance Dyn String   where dyn = S
+instance Dyn (D -> D) where dyn = F
+instance Dyn ((D, D) -> D) where dyn = F2
+
 main = do
-  print (B True)
-  print (B False)
+  print (dyn True)
+  print (dyn False)
   print (N 123)
-  print (S "hello")
+  print (dyn "hello")
   print show
-  print $ (N 2)    * (N 3)
-  print $ (S "hi") * (N 3)
+  print $ (N 2)      * (N 3)
+  print $ (dyn "hi") * (N 3)
+  print $ (dyn "hi") * (N 3) * (N 2) * (dyn "abc") -- "type error"
   -- print $ (B True) * (N 3) -- "type error"
+  print $ dyn True
